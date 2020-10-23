b50bb0730dfcd729402030f2a82c6e18c037ca33
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-23 19:08:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,6 +135,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages]
+    programmer_hash[:dennis_ritchie][:languages] << "GUI"
 
 end
